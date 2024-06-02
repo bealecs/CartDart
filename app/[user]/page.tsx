3ad4c3a4e32834237baca20ec687c54a76501727
bealecs <cliftonscottbeale@gmail.com>
2024-06-02 01:03:45 -0500

    starting to add dynamic routes for user pages

---
+++ app/[user]/page.tsx
@@ -0,0 +1,43 @@
+"use server";
+import { Navbar } from "@/components/Navbar";
+import MapComponent from "@/components/edit-profile-section/Geolocation/MapComponent";
+import { createClient } from "@/utils/supabase/server";
+import Image from "next/image";
+
+export default async function Page({ params }: { params: { user: string } }) {
+  const supabase = createClient();
+  console.log('params received:', params)
+  const { data, error } = await supabase
+    .from("profiles")
+    .select('*')
+    .eq("name", params.user);
+
+  if (error) {
+    console.log(error);
+    return <div>There was an error loading this profile, please go back</div>
+  }
+
+  if (!data || data.length === 0) {
+    return <div>No profile found.</div>;
+  }
+
+ return (
+    <div>
+      <Navbar />
+      {data.map((profile) => (
+        <div key={profile.id}>
+          <div className="flex">
+          <p className="items-center content-center mr-5">Name: {profile.name}</p>
+          <Image width={50} height={50} className="rounded" src={profile.pfp} alt={profile.name} />
+          </div>
+          <p className="my-5">Vendor Type: {profile.vendor_type}</p>
+          <div className="flex flex-col">
+            <p>Location:</p>
+            <MapComponent coordinates={profile.Latitude_Longitude_Location} />
+          </div>
+          {/* You can add more fields from the profile object here */}
+        </div>
+      ))}
+    </div>
+  );
+}
--- components/home-page-display/HomeDisplay.tsx
+++ components/home-page-display/HomeDisplay.tsx
@@ -28,37 +28,40 @@ export default async function HomeDisplay() {
               className="w-5/12 border-solid border-2 border-white m-5 flex justify-between"
               key={user.id}
             >
-              <div className="m-5 flex flex-col justify-evenly">
-                <div>
-                  <p className="underline">Name:</p>
-                  <div className="flex">
-                    <p className="items-center content-center">{user.name}</p>
-                    <Image
-                      height={60}
-                      width={60}
-                      src={user.pfp}
-                      alt="Profile picture for the vendor specified"
-                      className="rounded-full mx-5"
-                    />
+              <a href={`/${user.name}`}>
+                <div className="mx-auto items-center flex flex-col justify-evenly h-full">
+                  <div>
+                    <p className="underline">Name:</p>
+                    <div className="flex">
+                      <p className="items-center content-center">{user.name}</p>
+                      <Image
+                        height={60}
+                        width={60}
+                        src={user.pfp}
+                        alt="Profile picture for the vendor specified"
+                        className="rounded-full mx-5"
+                      />
+                    </div>
+                  </div>
+
+                  <div className="items-center mx-auto flex flex-col">
+                    <p className="underline">Vendor Cuisine:</p>
+                    <p>
+                      {!user.vendor_type
+                        ? "No vendor type found"
+                        : `${user.vendor_type}`}
+                    </p>
+                  </div>
+                  <div className="items-center mx-auto flex flex-col">
+                    <p className="underline">{"Today's Special:"}</p>
+                    <p className="items-center content-center mx-auto">
+                      {!user.special_today
+                        ? "The are currently no deals highlighted for this vendor"
+                        : user.special_today}
+                    </p>
                   </div>
                 </div>
-                <div>
-                  <p className="underline">Vendor Cuisine:</p>
-                  <p>
-                    {!user.vendor_type
-                      ? "No vendor type found"
-                      : `${user.vendor_type}`}
-                  </p>
-                </div>
-                <div>
-                  <p className="underline">{"Today's Special:"}</p>
-                  <p>
-                    {!user.special_today
-                      ? "The are currently no deals highlighted for this vendor"
-                      : user.special_today}
-                  </p>
-                </div>
-              </div>
+              </a>
               {user.Latitude_Longitude_Location ? (
                 <MapComponent coordinates={user.Latitude_Longitude_Location} />
               ) : (
