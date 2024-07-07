d5933aa90ba6e065bb4bfd6a3f3efbfef66eb5f7
bealecs <cliftonscottbeale@gmail.com>
2024-07-07 14:54:26 -0500

    cache buster for image retrieval / instant update gratification

--- components/edit-profile-section/pfp-section/PFP.tsx
+++ components/edit-profile-section/pfp-section/PFP.tsx
@@ -5,13 +5,14 @@ import FetchPFP from "./FetchPFP";
 export default async function PFP({ size }: {size: number}) {
 
   const pfp: string = await FetchPFP();
-
+  const timestamp = new Date().getTime();
+  
   return (
     <div>
       <Image
         width={size}
         height={size}
-        src={pfp ? pfp : "/default-pfp.svg"}
+        src={pfp ? pfp + "?v=" + timestamp : "/default-pfp.svg"}
         alt="User Profile Picture"
         className="rounded-full"
       />
